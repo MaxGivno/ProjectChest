a1a36494cb8c785f66af42ad42576055826dbb97
max_givno <max.givno@me.com>
2016-02-10 17:44:23 +0200

    Update

--- src/main/java/dev/maxgivno/projectchest/block/BlockProjectChest.java
+++ src/main/java/dev/maxgivno/projectchest/block/BlockProjectChest.java
@@ -1,10 +1,12 @@
 package dev.maxgivno.projectchest.block;
 
+import dev.maxgivno.projectchest.creativetab.CreativeTab;
 import net.minecraft.block.material.Material;
 
 public class BlockProjectChest extends BlockBase {
 
     public BlockProjectChest() {
         super(Material.wood);
+        this.setCreativeTab(CreativeTab.PCH_TAB);
     }
 }
